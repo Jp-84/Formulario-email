fd207e9e33fc194e5664abfd689bd7c27fc59e0a
Jp-84 <pro38jp@gmail.com>
2022-10-20 09:25:06 -0400

    Fin de Proyecto

--- js/app.js
+++ js/app.js
@@ -1,31 +1,106 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const email = {
+        email: '',        
+        asunto: '',
+        mensaje: ''
+    };
+    
+
+
     //Seleccionar los elemetos de la interfaz
     const inputEmail = document.querySelector('#email');
+    const inputCc = document.querySelector('#cc');
     const inputAsunto = document.querySelector('#asunto');    
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
+    
     
     // Asignar Eventos
 
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);    
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);    
+    inputMensaje.addEventListener('input', validar);
+    inputCc.addEventListener('input', validar);
+    
+    formulario.addEventListener('submit', enviarEmail);
+    
+    btnReset.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        resetFormulario();
+    })
+    
+    function enviarEmail(e) {
+        e.preventDefault();
+        
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+        
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden'); 
+            
+            resetFormulario();
+            
+            // Crear un alerta 
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+            
+        }, 3000);
+
+    }
+
     
     function validar(e) {
 
-        if (e.target.value.trim() === '') {
+        
+        if (e.target.value.trim() === '' && e.target.id !== 'cc') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        } else {
-            console.log('Si hay algo...');
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        } 
+        
+        if (e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta(`El email no es valido`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
         }
+
+        if (e.target.id === 'cc' && !validarEmail(e.target.value)){
+            mostrarAlerta(`El email no es valido`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        
+        limpiarAlerta(e.target.parentElement);
+
+        // Asiganar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        // Comprobar el objeto de email
+        comprobarEmail();
+
     }
+
+
 
     function mostrarAlerta(mensaje, referencia) {
 
-        // Comprueba si ya existe una alerta
-        const alerta = document.querySelector('bg-red-600')
+        limpiarAlerta(referencia);
 
 
         // Generar alerta HTML
@@ -37,6 +112,41 @@ document.addEventListener('DOMContentLoaded', function () {
         referencia.appendChild(error)
     }
 
+    function limpiarAlerta(referencia) {
+        
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if (alerta) {
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email) {
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarEmail() {
+        if (Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;  
+            return; 
+        }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+    }
+    
+    function resetFormulario() {
+        // Reiniciar el Objeto
+        email.email = '';
+        email.cc = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+    }
     
     
 });
